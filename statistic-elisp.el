2394989c71947b6ca8dd1b4fd35b7c90b8fd0ac2
MacPapo <jacopocostantini32@gmail.com>
2022-10-12 13:07:13 +0200

    Functions now output the number and not only the message

--- statistic-elisp.el
+++ statistic-elisp.el
@@ -5,25 +5,29 @@
 
 ;;; Code:
 
+(require 'cl-lib)
+
 (defun fact (n)
   "Calculate the factorial of a number N."
   (if (<= n 1)
       1
-    (* n
-       (fact (- n 1)))))
+    (* n (fact (- n 1)))))
 
 (defun perm-no-rip (n)
   "Calculate the number N of simple permutations."
   (interactive "nInsert a Num: ")
-  (message "Result: %d" (fact n)))
+  (let ((res (fact n)))
+    (message "Result: %d" res)))
 
 (defun perm-rip (n nr)
   "Calculate the number N of permutations with repetitions NR."
   (interactive "nInsert a Num: \nnInsert the number of repetitions: ")
-  (let ((prod-rep (apply '* (mapcar (lambda (n)
-                                      (fact n))
-                                    (statistic-compose-list nr)))))
-    (message "Result: %d" (/ (fact n) prod-rep))))
+  (let* ((prod-rep (apply '* (mapcar (lambda (n)
+                                       (fact n))
+                                     (statistic-compose-list nr))))
+         (res (/ (fact n) prod-rep)))
+    (message "Result: %d" res)
+    res))
 
 (defun statistic-compose-list (n)
   "Create a list of numbers with length N."
@@ -35,32 +39,40 @@
       (cl-decf n))
     new-list))
 
-(defun disp-no-rip (n k)
-  "Calculate the number N of simple arrangements K."
-  (interactive
-   "Give me the number of elements: \nnGive me the number of combo: ")
-  (message "Result: %d" (/ (fact n)
-                           (fact (- n k)))))
-
-(defun disp-rip (n k)
-  "Calculate the number N of arrangements with K repetitions."
+(defun disp-no-rip (n r)
+  "Calculate the number N of simple arrangements R."
   (interactive
    "nGive me the number of elements: \nnGive me the number of combo: ")
-  (message "Result: %d" (expt n k)))
+  (let ((res (/ (fact n)
+                (fact (- n r)))))
+    (message "Result: %d" res)
+    res))
+
+(defun disp-rip (n r)
+  "Calculate the number N of arrangements with R repetitions."
+  (interactive
+   "nGive me the number of elements: \nnGive me the number of combo: ")
+  (let ((res (expt n r)))
+    (message "Result: %d" res)
+    res))
 
 (defun combo-no-rip (n r)
   "Calculate the number N of simple combinations R."
   (interactive
    "nGive me the number of elements: \nnGive me the number of combo: ")
-  (message "Result: %d" (/ (fact n)
-                           (* (fact r) (fact (- n r))))))
+  (let ((res (/ (fact n)
+                (* (fact r) (fact (- n r))))))
+    (message "Result: %d" res)
+    res))
 
 (defun combo-rip (n r)
   "Calculate the number N of combinations with R repetitions."
   (interactive
    "nGive me the number of elements: \nnGive me the number of combo: ")
-  (message "Result: %d" (/ (fact (- (+ n r) 1))
-                           (* (fact r) (fact (- n 1))))))
+  (let ((res (/ (fact (- (+ n r) 1))
+                (* (fact r) (fact (- n 1))))))
+    (message "Result: %d" res)
+    res))
 
 (provide 'statistic-elisp)
 ;;; statistic-elisp.el ends here
