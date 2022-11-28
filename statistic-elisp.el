7624fad8f6d0cc0447e4756d8140dc2fa639efba
MacPapo <jacopocostantini32@gmail.com>
2022-11-28 10:49:59 +0100

    New formulas

--- statistic-elisp.el
+++ statistic-elisp.el
@@ -17,7 +17,7 @@
   "Calculate the number N of simple permutations."
   (interactive "nInsert a Num: ")
   (let ((res (fact n)))
-    (message "Result: %d" res)))
+    (message "Result: %.7f" res)))
 
 (defun perm-rip (n nr)
   "Calculate the number N of permutations with repetitions NR."
@@ -25,8 +25,8 @@
   (let* ((prod-rep (apply '* (mapcar (lambda (n)
                                        (fact n))
                                      (statistic-compose-list nr))))
-         (res (/ (fact n) prod-rep)))
-    (message "Result: %d" res)
+         (res (/ (float (fact n)) prod-rep)))
+    (message "Result: %.7f" prod-rep)
     res))
 
 (defun statistic-compose-list (n)
@@ -43,26 +43,26 @@
   "Calculate the number N of simple arrangements R."
   (interactive
    "nGive me the number of elements: \nnGive me the number of combo: ")
-  (let ((res (/ (fact n)
+  (let ((res (/ (float (fact n))
                 (fact (- n r)))))
-    (message "Result: %d" res)
+    (message "Result: %.7f" res)
     res))
 
 (defun disp-rip (n r)
   "Calculate the number N of arrangements with R repetitions."
   (interactive
    "nGive me the number of elements: \nnGive me the number of combo: ")
-  (let ((res (expt n r)))
-    (message "Result: %d" res)
+  (let ((res (expt (float n) r)))
+    (message "Result: %.7f" res)
     res))
 
 (defun combo-no-rip (n r)
   "Calculate the number N of simple combinations R."
   (interactive
    "nGive me the number of elements: \nnGive me the number of combo: ")
-  (let ((res (/ (fact n)
-                (* (fact r) (fact (- n r))))))
-    (message "Result: %d" res)
+  (let ((res (/ (float (fact n))
+                (* (float (fact  r)) (fact (float (- n r)))))))
+    (message "Result: %.7f" res)
     res))
 
 (defun combo-rip (n r)
@@ -71,7 +71,81 @@
    "nGive me the number of elements: \nnGive me the number of combo: ")
   (let ((res (/ (fact (- (+ n r) 1))
                 (* (fact r) (fact (- n 1))))))
-    (message "Result: %d" res)
+    (message "Result: %.7f" res)
+    res))
+
+(defun dbinom (x size prob)
+  "Calculate the binom distribution of X with SIZE and PROB."
+  (interactive
+   "nGive me x value: \nnGive me the size: \nnGive me the probability: ")
+  (let* ((bin (combo-no-rip size x))
+	 (res (* bin
+		 (expt (float prob) x) 
+		 (expt (flaot (- 1 prob)) (- size x)))))
+    (message "Result: %.7f" res)
+    res))
+
+(defun pbinom (q size prob)
+  "Calc Q SIZE PROB."
+  (interactive
+   "nGive me x value: \nnGive me the size: \nnGive me the probability: ") 
+  (if (< q 0)
+      0
+    (let ((res (+ (dbinom q size prob) 
+		  (pbinom (- q 1) size prob))))
+      (message "Result: %.8f" res)
+      res)))
+
+(defun dpois (x lamb)
+  "Calc X LAMB."
+  (interactive
+   "nGive x: \nnGive lamb: ")
+  (let ((res (* (/ (expt (float lamb) x)
+		   (fact (float x)))
+		(exp (- lamb)))))
+    (message "Result: %.7f" res)
+    res))
+
+(defun ppois (q lamb)
+  "Calc Q LAMB."
+  (interactive
+   "nGive x: \nnGive lamb: ")
+  (if (< q 0)
+      0
+    (let ((res (+ (dpois q lamb) 
+		  (ppois (- q 1) lamb))))
+      (message "Result: %.7f" res)
+      res)))
+
+(defun neg-ppois (q lamb)
+  "Calc Q LAMB."
+  (interactive
+   "nGive q: \nnGive lamb: ")
+  (let ((res (- 1 (ppois q lamb))))
+    (message "Result: %.7f" res)
+    res))
+
+(defun appros-ppois (x size prob)
+  "Calc X SIZE PROB."
+  (interactive
+   "nGive me x value: \nnGive me the size: \nnGive me the probability: ")
+  (if (< x 0)
+      0
+    (let* ((lamb (* size prob))
+	   (res (+ (* (float (exp (- lamb)))
+		      (/ (expt (float lamb) x)
+			 (fact (float x))))
+		   (appros-ppois (- x 1) size prob))))
+      (message "Result: %.8f" res)
+      res)))
+
+(defun dgeom (x prob)
+  "Distribuzione Geometrica X PROB."
+  (interactive
+   "nGive x: \nnGive prob: ")
+  (let ((res (* (expt (float (- 1 prob)) (- x 1))
+		prob)))
+    (message "Result: %.8f" res)
     res))
 
 (provide 'statistic-elisp)
